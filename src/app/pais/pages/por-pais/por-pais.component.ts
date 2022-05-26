5690d866bf68fda5233adee54670b955554ee3f8
Heriberto Lara <jslj@gft.com>
2022-05-26 10:49:39 -0500

    app de paises terminada

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,16 +6,23 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
-    
+    `
+    li{
+      cursor: pointer
+    }
+    `
   ]
 })
 export class PorPaisComponent{
-  termino:string = ""
-  hayError: boolean = false
-  paises: Country[]=[]
+  termino:string = "";
+  hayError: boolean = false;
+  paises: Country[]=[];
+  paisesSugeridos:Country[] = [];
+  mostrarSugerencias: boolean = false
   constructor( private paisService:PaisService) { }
 
   buscar( termino:string ) {
+    this.mostrarSugerencias = false
     this.hayError= false
     this.termino = termino
     console.log(this.termino)
@@ -29,7 +36,18 @@ export class PorPaisComponent{
       })
   }
   sugerencias(termino:string){
-    this.hayError = false
+    this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    this.paisService.buscarPais( termino).subscribe( 
+      paises => this.paisesSugeridos = paises.splice(0,5),
+      err=>  this.paisesSugeridos = []
+    )
+  }
+
+  buscarSugerido( termino:string ){
+    this.buscar(termino);
+
   }
  
 
